3b4c4ce4687efb8ee9bf45afcd93c7ecb8351e0b
Felipe Castro <felipecastrolb@gmail.com>
2024-05-24 14:57:11 -0300

    Alteracao nas pastas

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const botaoComecar = document.getElementById('botao-comecar');
 
 function irQuiz(){
-    window.location.href = "../quiz/quiz.html";
+    window.location.href = "pages/quiz/quiz.html";
 }
 
 botaoComecar.addEventListener('click', irQuiz);
