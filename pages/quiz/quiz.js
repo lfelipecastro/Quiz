717c5a5548ac8db40ab1904ef8b03aebf7b94287
Felipe Castro <felipecastrolb@gmail.com>
2024-05-24 13:21:24 -0300

    Projeto finalizado

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -8,10 +8,10 @@ const botaoVoltar = document.getElementById('botao-voltar');
 
 const perguntaUm = {
     pergunta : '1. Como podemos definir um Algoritmo?',
-    altA : 'Sequência de comandos ordenados, infinitos e ambíguos que são empregados para executar uma tarefa.',
-    altB : 'Sequência de instruções ordenadas, infinitas e não-ambíguas que são empregadas para executar uma tarefa.',
-    altC : 'Sequência de instruções ordenadas, finitas e não-ambíguas que são empregadas para executar uma tarefa.',
-    altD : 'Sequência de comandos ordenados, infinitos e não-ambíguos que são empregados para executar uma tarefa.',
+    altA : 'Sequência de comandos ordenados, infinitos e ambíguos que são empregados para executar uma tarefa',
+    altB : 'Sequência de instruções ordenadas, infinitas e não-ambíguas que são empregadas para executar uma tarefa',
+    altC : 'Sequência de instruções ordenadas, finitas e não-ambíguas que são empregadas para executar uma tarefa',
+    altD : 'Sequência de comandos ordenados, infinitos e não-ambíguos que são empregados para executar uma tarefa',
     respCerta : 3,
 }
 
@@ -25,7 +25,16 @@ const perguntaDois = {
 }
 
 const perguntaTres = {
-    pergunta : '3.7. É possível ordenar valores numéricos já armazenados em um vetor de números decimais?',
+    pergunta : '3. Quais são todos os estados possíveis que um tipo lógico pode assumir?',
+    altA : 'Verdadeiro, Duvidoso',
+    altB : 'Verdadeiro, Errado',
+    altC : 'Incerto, Certo',
+    altD : 'Verdadeiro, Falso',
+    respCerta : 4,
+}
+
+const perguntaQuatro = {
+    pergunta : '4. É possível ordenar valores numéricos já armazenados em um vetor de números decimais?',
     altA : 'Não, somente é possível ter valores em ordem se os valores forem inseridos em ordem',
     altB : 'Sim, mas somente se os números forem inteiros. Números decimais não podem ser ordenados',
     altC : 'Sim, mas para isso é necessário usar um algoritmo para ordenação de vetores',
@@ -33,11 +42,66 @@ const perguntaTres = {
     respCerta : 3,
 }
 
+const perguntaCinco = {
+    pergunta : '5. Qual dos seguintes itens é melhor representado por um valor constante em um algoritmo?',
+    altA : 'Salário de um Funcionário',
+    altB : 'Valor da Temperatura no final de semana',
+    altC : 'Valor matemático de Pi',
+    altD : 'Distância da Terra à Lua',
+    respCerta : 3,
+}
+
+const perguntaSeis = {
+    pergunta : '6. O que é Compilação?',
+    altA : 'Comandos contidos no código-fonte são executados à medida em que são traduzidos, sendo necessária a interpretação toda vez que se quiser rodar o programa',
+    altB : 'Tradução do código-fonte em um ou mais arquivos que podem ser armazenados e executados quantas vezes se desejar, sem necessitar de nova compilação',
+    altC : 'Execução das instruções contidas em um script contendo o código-fonte, de forma sequencial',
+    altD : 'Organização do código-fonte em uma pasta contendo arquivos em ordem alfabética',
+    respCerta : 2,
+}
+
+const perguntaSete = {
+    pergunta : '7. Qual dos seguintes itens mostra um laço for (para) escrito corretamente?',
+    altA : 'para (con = 1; con < 10; con–) { instruções }',
+    altB : 'para (con = 10; con >= 1; con++) { instruções }',
+    altC : 'para (con = 10; con < 10; con–) { instruções }',
+    altD : 'para (con = 1; con <= 10; con++) { instruções }',
+    respCerta : 4,
+}
+
+const perguntaOito = {
+    pergunta : '8. O que é uma Variável?',
+    altA : 'Local na memória RAM do computador utilizado para armazenar temporariamente dados que são utilizados pelo programa',
+    altB : 'Estado lógico de uma constante, que pode variar entre verdadeiro e falso',
+    altC : 'Valor que varia conforme passa o tempo, sendo sempre modificado conforme o programa vai sendo executado',
+    altD : 'Valor armazenado em locais variáveis no computador, como HD, RAM ou Memória Cache',
+    respCerta : 1,
+}
+
+const perguntaNove = {
+    pergunta : '9. Qual das seguintes afirmações sobre arrays (vetores) é verdadeira?',
+    altA : 'Podemos acessar todos os elementos individuais de uma array por meio de uma posição de índice, começando sempre em 1',
+    altB : 'Um array é uma estrutura que pode armazenar uma série de elementos de dados de mesmo tipo',
+    altC : 'Um array é uma estrutura que permite armazenar até 10 elementos de dados, sempre do mesmo tipo',
+    altD : 'Podemos acessar os elementos individuais de uma array por meio de uma posição de índice, começando em A',
+    respCerta : 2,
+}
+
+const perguntaDez = {
+    pergunta : '10. Para que serve uma biblioteca?',
+    altA : 'Uma biblioteca é um arquivo que organiza código pré-definido para o uso em aplicações',
+    altB : 'Para modularizar o desenvolvimento de estruturas de comparação e repetição',
+    altC : 'Uma biblioteca é um programa que permite escrever e testar código-fonte, também conhecida como IDE',
+    altD : 'Para que os programadores possam consultar a documentação da linguagem com facilidade',
+    respCerta : 1,
+}
+
 let indice = 0;
 let index = 0;
 let valorRespCertas = 0;
-const perguntasArray = [perguntaUm, perguntaDois, perguntaTres];
-const respInseridasArray = [0,0,0];
+const perguntasArray = [perguntaUm, perguntaDois, perguntaTres, perguntaQuatro, perguntaCinco, perguntaSeis, 
+    perguntaSete, perguntaOito, perguntaNove, perguntaDez];
+const respInseridasArray = [0,0,0,0,0,0,0,0,0,0];
 
 function iniciarQuiz(){
     pergunta.innerText = perguntasArray[index].pergunta;
@@ -57,17 +121,22 @@ function valorPontuacao(){
 }
 
 function prosseguirQuiz(){
-    if(index >= perguntasArray.length - 1){
-        valorPontuacao();
-        window.location.href = "../resultado/resultado.html?acertos="+valorRespCertas;
-        index = 0;
-    }else{
-        index += 1;
-        iniciarQuiz();
-    }
+    limpaAlternativas();
+    if(indice != 0)
+        if(index >= perguntasArray.length - 1){
+            valorPontuacao();
+            window.location.href = "../resultado/resultado.html?acertos="+valorRespCertas;
+            index = 0;
+            indice = 0;
+        }else{
+            index += 1;
+            iniciarQuiz();
+            indice = 0;
+        }
 }
 
 function voltar(){
+    limpaAlternativas();
     if(index <= 0){
         index = 0;
         history.back();
@@ -77,24 +146,51 @@ function voltar(){
     }
 }
 
+function limpaAlternativas(){
+    altA.classList.remove('botao-quiz-pressionado');
+    altA.classList.add('botao-quiz');
+    altB.classList.remove('botao-quiz-pressionado');
+    altB.classList.add('botao-quiz');
+    altC.classList.remove('botao-quiz-pressionado');
+    altC.classList.add('botao-quiz');
+    altD.classList.remove('botao-quiz-pressionado');
+    altD.classList.add('botao-quiz');
+}
+
 function selecionaA(){
     indice = 1;
     respInseridasArray[index] = indice;
+
+    limpaAlternativas();
+    altA.classList.remove('botao-quiz');
+    altA.classList.add('botao-quiz-pressionado');
 }
 
 function selecionaB(){
     indice = 2;
     respInseridasArray[index] = indice;
+
+    limpaAlternativas();
+    altB.classList.remove('botao-quiz');
+    altB.classList.add('botao-quiz-pressionado');
 }
 
 function selecionaC(){
     indice = 3;
     respInseridasArray[index] = indice;
+
+    limpaAlternativas();
+    altC.classList.remove('botao-quiz');
+    altC.classList.add('botao-quiz-pressionado');
 }
 
 function selecionaD(){
     indice = 4;
     respInseridasArray[index] = indice;
+
+    limpaAlternativas();
+    altD.classList.remove('botao-quiz');
+    altD.classList.add('botao-quiz-pressionado');
 }
 
 iniciarQuiz();
--- pages/resultado/resultado.js
+++ pages/resultado/resultado.js
@@ -1,6 +1,7 @@
 const botaoRecomecar = document.getElementById('botao-recomecar');
 const pontuacao = document.getElementById('pontuacao');
 const mensagem = document.getElementById('mensagem');
+const respostas = document.getElementById('respostas');
 let pontos = "0";
 
 function voltarComeco(){
@@ -11,14 +12,15 @@ function mostrarResultado(){
     const urlParams = new URLSearchParams(window.location.search);
     pontos = urlParams.get("acertos");
     pontuacao.innerText = "Sua pontuação: "+pontos;
+    respostas.innerText = "Alternativas corretas: 1-C, 2-D, 3-D, 4-C, 5-C, 6-B, 7-D, 8-A, 9-B, 10-A";
 
     if(pontos >= 0 && pontos <= 3){
-        mensagem.innerText = "Estude mais sobre programação!";
+        mensagem.innerText = "Estude muito mais sobre programação!";
     }else if(pontos >= 4 && pontos <= 6){
         mensagem.innerText = "Você está no caminho certo, é só estudar um pouco mais!";
     }else if(pontos >= 7 && pontos <= 9){
-        mensagem.innerText = "Você é demais! Mantenha esse ritmo.";
-    }else if(pontos === 10){
+        mensagem.innerText = "Você é demais! Mantenha esse ritmo de aprendizado.";
+    }else if(pontos == 10){
         mensagem.innerText = "Você é o(a) Eistein da programação!";
     }
 }
